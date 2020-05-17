196bebb5c6598f9260319f59d787052dc863a754
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-05-17 02:02:03 -0700

    rfct

--- controller/whatsapp.py
+++ controller/whatsapp.py
@@ -7,43 +7,19 @@ class WhatsappClassifier():
 
     def whatsapp_reply(self, sample):
 
+        HAPPY_DOG_URL = "https://t1.ea.ltmcdn.com/pt/images/4/2/0/cachorro_feliz_recomendacoes_gerais_23024_orig.jpg"
+        #sad_dog_url = "https://i.pinimg.com/originals/e4/a5/33/e4a533fca82e142230182129793682b6.jpg"
+
         # Classificador
         cl = Classifier('./models/')
         result = cl.make_classification(sample)
 
         # Cria a resposta
         resp = MessagingResponse()
-        resp.message(str(result))
-
-
-        return str(resp)
-
-
-    def whatsapp_reply_2(self, sample):
-
-        #Imagens provisórias para o retorno via Whatsapp
-        happy_dog_url = "https://t1.ea.ltmcdn.com/pt/images/4/2/0/cachorro_feliz_recomendacoes_gerais_23024_orig.jpg"
-        #sad_dog_url = "https://i.pinimg.com/originals/e4/a5/33/e4a533fca82e142230182129793682b6.jpg"
-
-        resp = MessagingResponse()
-
-        cl = Classifier('./models/')
-        result = cl.make_classification(sample)
-        
-        msg = resp.message(result[0])
-        msg.media(happy_dog_url)
+        msg = resp.message(str(result))
+        msg.media(HAPPY_DOG_URL)
 
         return str(resp)
-
-
-
-
-
-
-
-
-
-
 
 # Cria um bot "eco" para WhatsApp
 # Para acessá-lo a pessoa deve enviar a mensagem "join around-south" sem aspas para o número +1 415 523 8886
