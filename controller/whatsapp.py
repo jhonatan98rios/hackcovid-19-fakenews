4cd60aedbfe87187404891e4af63c1ec5f21371d
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-05-17 02:44:31 -0700

    Gif implemeentation

--- controller/whatsapp.py
+++ controller/whatsapp.py
@@ -7,10 +7,10 @@ class WhatsappClassifier():
 
     def whatsapp_reply(self, sample):
 
-        REALLY_TRUTH_URL = "https://t1.ea.ltmcdn.com/pt/images/4/2/0/cachorro_feliz_recomendacoes_gerais_23024_orig.jpg"
-        MAYBE_TRUTH_URL = "https://t1.ea.ltmcdn.com/pt/images/4/2/0/cachorro_feliz_recomendacoes_gerais_23024_orig.jpg"
-        REALLY_FALSE_URL = "https://i.pinimg.com/originals/e4/a5/33/e4a533fca82e142230182129793682b6.jpg"
-        MAYBE_FALSE_URL = "https://i.pinimg.com/originals/e4/a5/33/e4a533fca82e142230182129793682b6.jpg"
+        REALLY_TRUTH_URL = "https://i.pinimg.com/originals/83/2b/34/832b34e5e61f5e7d45da478f2c545e53.gif"
+        MAYBE_TRUTH_URL = "https://media.tenor.com/images/1dd2bc5ce366262629bd8d7d6b0e674a/tenor.gif"
+        MAYBE_FALSE_URL = "https://loonylabs.files.wordpress.com/2016/08/dog-staring-at-cupcakes-gif.gif"
+        REALLY_FALSE_URL = "https://4.bp.blogspot.com/-0RRfcGdCTiI/VquKv3DhdmI/AAAAAAAB1oo/HdknqIR9tl0/s1600/5a991a2337f9dbd0feb3f648264c421c.gif"
 
         # Classificador
         cl = Classifier('./models/')
@@ -19,12 +19,12 @@ class WhatsappClassifier():
         # Define a imagem
         media = REALLY_TRUTH_URL
         media = MAYBE_TRUTH_URL if result[0] == 'NAO SEI, ESSA NOTICIA TALVEZ SEJA FALSA' else media
-        media = REALLY_FALSE_URL if result[0] == 'NAO SEI, ESSA NOTICIA TALVEZ SEJA VERDADEIRA' else media
         media = MAYBE_FALSE_URL if result[0] == 'ESSA NOTICIA PARECE VERDADEIRA' else media
+        media = REALLY_FALSE_URL if result[0] == 'NAO SEI, ESSA NOTICIA TALVEZ SEJA VERDADEIRA' else media
 
         # Cria a resposta
         resp = MessagingResponse()
-        msg = resp.message(str(result))
+        msg = resp.message(str(result[0]))
         msg.media(media)
 
         return str(resp)
