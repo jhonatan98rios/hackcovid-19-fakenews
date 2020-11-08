097ce64df7fafe0eb94bd3b4f9766e921f5c90fb
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-11-08 01:28:53 -0200

    db migration

--- databases/db.py
+++ databases/db.py
@@ -4,10 +4,11 @@ from databases.auth import dbuser, dbpassword
 class DBConnection:
     def __init__(self):
         try:
-            self.client = pymongo.MongoClient(f"mongodb://{dbuser}:{dbpassword}@ds163757.mlab.com:63757/heroku_9407v38l?retryWrites=false")
-            #self.database = self.client["HackCovid-19"]  # Access DataBase
-            self.database = self.client["heroku_9407v38l"]  # Access DataBase
-            self.collection = self.database["Collection_01"]  # Access collection
+            self.client = pymongo.MongoClient("mongodb+srv://<username>:<password>@cluster0.t0gwi.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority")
+            self.database = self.client["SmartGadget"]
+            self.collection = self.collection["phrases"]
+
+
         except pymongo.errors.AutoReconnect as e:
             print(f"Could not connect to server: {e}")
 
@@ -27,13 +28,3 @@ class DBConnection:
             }
             array.append(document)
         return array 
-
-
-# Heroku connection string: mongodb://<dbuser>:<dbpassword>@ds163757.mlab.com:63757/heroku_9407v38l
-# databasename: heroku_9407v38l
-# collection: Collection_01
-# user: heroku_9407v38ll
-# passw: P$yJb2hdi$wW35u
-
-# Shell
-# mongo ds163757.mlab.com:63757/heroku_9407v38l -u heroku_9407v38ll -p P$yJb2hdi$wW35u
